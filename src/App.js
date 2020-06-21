7f243a88d6efdafb74fa7a28b2a3ba7dfcdb9dcc
monir-007 <monir.hossain@northsouth.edu>
2020-06-21 16:17:16 +0600

    first commit

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React, {Component} from 'react';
+import './App.scss';
+import Navbar from './components/Navbar/navbar';
+import TourList from "./components/TourList";
+
+class App extends Component {
+  render(){
+    return (
+      <React.Fragment>
+        <Navbar />
+        <TourList />
+      </React.Fragment>
+
+    );
+
+  }
+
+}
+
+export default App;
---
+++ src/tourData.js
@@ -0,0 +1,34 @@
+export const tourData = [
+  {
+    id: 1,
+    city: "new york",
+    img: "./img/newyork.jpeg",
+    name: "new york bridge tour",
+    info:
+      "New York City (NYC), often called New York (NY), is the most populous city in the United States. With an estimated 2019 population of 8,336,817 distributed over about 302.6 square miles (784 km2), New York is also the most densely populated major city in the United States.Located at the southern tip of the U.S. state of New York, the city is the center of the New York metropolitan area, the largest metropolitan area in the world by urban landmass. With almost 20 million people in its metropolitan statistical area and approximately 23 million in its combined statistical area, it is one of the world's most populous megacities. New York City has been described as the cultural, financial, and media capital of the world, significantly influencing commerce, entertainment, research, technology, education, politics, tourism, art, fashion, and sports. Home to the headquarters of the United Nations, New York is an important center for international diplomacy."
+  },
+  {
+    id: 2,
+    city: "paris",
+    img: "./img/paris.jpeg",
+    name: "paris lights tour",
+    info:
+      "Paris, the City of Light is the world's most visited capital. The city is proud of its many monuments from the iconic Eiffel Tower to the lofty Notre-Dame cathedral and the majestic Arc de Triomphe. No doubt this is Europe's most enchanting capital! Know about the romantic destinations of Europe."
+  },
+  {
+    id: 3,
+    city: "london",
+    img: "./img/london.jpeg",
+    name: "london royal palace tour",
+    info:
+      "London is famous for more than its magnificent ancient buildings, the Palace of Westminster, Trafalgar Square, Tower of London, London Eye, Art Galleries, and Big Ben. As the capital city of the United Kingdom of Great Britain and Northern Ireland, London is well known for influencing other cities of the world since the Middle Ages. London is the city of dreams.  When visitors stroll around London they get excited to find so many magnificent things. Some people just stand, stare, and start dreaming because London is a city of dreams that arouses feelings of joy and curiosity."
+  },
+  {
+    id: 4,
+    city: "tokyo",
+    img: "./img/tokyo.jpeg",
+    name: "tokyo sushi tour",
+    info:
+      "Tokyo has been a well-traveled destination for decades. Cherry blossoms, Mt. Fuji, and sushi grace the postcards and photographs. This thriving, colorful city is famous for many more reasons than one. Tokyo boasts so many noteworthy attractions that it can be difficult to know what it was famous for first. If you plan on visiting this famous city, here are some of the top 7 things Tokyo is famous for.Home to the busiest railway stations on the planet, Shibuya Crossing has a reputation for being a bustling hub. This region is hugely populated, and is a hot spot for nightlife and youth culture. One of the top attractions is watching the street lights change at the main traffic crossing. Vehicles are stopped in all directions, allowing pedestrians to completely take over the entire crosswalk. There’s a Starbucks that overlooks the scene, so make sure to get up there and have a coffee while looking out below."
+  }
+];
