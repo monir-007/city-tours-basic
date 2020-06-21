e033a0d6cfd23b79f253744f4e041d4c93a42b0d
monir-007 <monir.hossain@northsouth.edu>
2020-06-21 17:46:41 +0600

    add tour data

--- src/tourData.js
+++ src/tourData.js
@@ -1,11 +1,11 @@
 export const tourData = [
   {
     id: 1,
-    city: "new york",
-    img: "./img/newyork.jpeg",
-    name: "new york bridge tour",
+    city: "barcelona",
+    img: "./img/barcelona.jpg",
+    name: "tour camp nou",
     info:
-      "New York City (NYC), often called New York (NY), is the most populous city in the United States. With an estimated 2019 population of 8,336,817 distributed over about 302.6 square miles (784 km2), New York is also the most densely populated major city in the United States.Located at the southern tip of the U.S. state of New York, the city is the center of the New York metropolitan area, the largest metropolitan area in the world by urban landmass. With almost 20 million people in its metropolitan statistical area and approximately 23 million in its combined statistical area, it is one of the world's most populous megacities. New York City has been described as the cultural, financial, and media capital of the world, significantly influencing commerce, entertainment, research, technology, education, politics, tourism, art, fashion, and sports. Home to the headquarters of the United Nations, New York is an important center for international diplomacy."
+      "Barcelona came in at no. 9 on Traveler's readers’ choice list of the top 25 cities in 2014. The beachfront city beloved by art lovers is a perennial hit with travelers, with 7.37 million people passing through this year. Despite being lower on the list than Istanbul, Barcelona wins out in the amount of tourist money being spent: $11.2 billion compared to Istanbul’s $9.4 billion."
   },
   {
     id: 2,
@@ -33,50 +33,35 @@ export const tourData = [
   },
   {
     id: 5,
-    city: "paris",
-    img: "./img/paris.jpeg",
-    name: "paris lights tour",
+    city: "new york",
+    img: "./img/newyork.jpeg",
+    name: "new york bridge tour",
     info:
-      "Paris, the City of Light is the world's most visited capital. The city is proud of its many monuments from the iconic Eiffel Tower to the lofty Notre-Dame cathedral and the majestic Arc de Triomphe. No doubt this is Europe's most enchanting capital! Know about the romantic destinations of Europe."
+      "New York City (NYC), often called New York (NY), is the most populous city in the United States. With an estimated 2019 population of 8,336,817 distributed over about 302.6 square miles (784 km2), New York is also the most densely populated major city in the United States.Located at the southern tip of the U.S. state of New York, the city is the center of the New York metropolitan area, the largest metropolitan area in the world by urban landmass. With almost 20 million people in its metropolitan statistical area and approximately 23 million in its combined statistical area, it is one of the world's most populous megacities. New York City has been described as the cultural, financial, and media capital of the world, significantly influencing commerce, entertainment, research, technology, education, politics, tourism, art, fashion, and sports. Home to the headquarters of the United Nations, New York is an important center for international diplomacy."
   },
   {
     id: 6,
-    city: "paris",
-    img: "./img/paris.jpeg",
-    name: "paris lights tour",
+    city: "austria",
+    img: "./img/vienna.jpg",
+    name: "beautiful view of karlskirche",
     info:
-      "Paris, the City of Light is the world's most visited capital. The city is proud of its many monuments from the iconic Eiffel Tower to the lofty Notre-Dame cathedral and the majestic Arc de Triomphe. No doubt this is Europe's most enchanting capital! Know about the romantic destinations of Europe."
+      "Vienna dropped one spot from last year, with just over 6 million tourists visiting. Still, it’s growing in visitor spending, beating out both Milan (no. 13 on MasterCard’s list) and Berlin (which didn’t make the cut). There’s much more to Austria’s capital than the classical music and art that made it famous—it’s one of the most eco-friendly cities in Europe."
   },
   {
     id: 7,
-    city: "paris",
-    img: "./img/paris.jpeg",
-    name: "paris lights tour",
+    city: "rome",
+    img: "./img/rome.jpg",
+    name: "rome to venice",
     info:
-      "Paris, the City of Light is the world's most visited capital. The city is proud of its many monuments from the iconic Eiffel Tower to the lofty Notre-Dame cathedral and the majestic Arc de Triomphe. No doubt this is Europe's most enchanting capital! Know about the romantic destinations of Europe."
+      "Milan edged out Rome as the most popular destination in Italy, with 6.82 million visitors to Rome’s 6.79 million. Still, the Eternal City had an uptick in both the number of tourists and the amount of money spent per person, and it remains beloved by Conde Nast Traveler readers, who voted it their seventh-favorite city in the world."
   },
   {
     id: 8,
-    city: "paris",
-    img: "./img/paris.jpeg",
-    name: "paris lights tour",
+    city: "Istanbul",
+    img: "./img/Istanbul.jpg",
+    name: "Eye catching Bildrone",
     info:
-      "Paris, the City of Light is the world's most visited capital. The city is proud of its many monuments from the iconic Eiffel Tower to the lofty Notre-Dame cathedral and the majestic Arc de Triomphe. No doubt this is Europe's most enchanting capital! Know about the romantic destinations of Europe."
-  },
-  {
-    id: 9,
-    city: "paris",
-    img: "./img/paris.jpeg",
-    name: "paris lights tour",
-    info:
-      "Paris, the City of Light is the world's most visited capital. The city is proud of its many monuments from the iconic Eiffel Tower to the lofty Notre-Dame cathedral and the majestic Arc de Triomphe. No doubt this is Europe's most enchanting capital! Know about the romantic destinations of Europe."
-  },
-  {
-    id: 10,
-    city: "paris",
-    img: "./img/paris.jpeg",
-    name: "paris lights tour",
-    info:
-      "Paris, the City of Light is the world's most visited capital. The city is proud of its many monuments from the iconic Eiffel Tower to the lofty Notre-Dame cathedral and the majestic Arc de Triomphe. No doubt this is Europe's most enchanting capital! Know about the romantic destinations of Europe."
+      "Istanbul is at the crossroads of Europe and Asia, and its ranking on the list this year reflects its central location. Fifty-five percent of tourists in Istanbul come from outside Europe; the city is also becoming popular as an extended layover spot—it was the fastest growing international air hub in 2014."
   }
+
 ];
